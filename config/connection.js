f73babc351f37786788729ac1b32b78dc7c424c8
Timothy Charette <thepubliccoder@gmail.com>
2019-01-08 22:15:27 -0500

    final

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,17 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  })
+}
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
